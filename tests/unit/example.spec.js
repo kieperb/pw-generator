43087498c1b0e545290140fafaf061abb2ef1fc6
kieperb <>
2022-01-17 01:17:44 +0100

    init

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,15 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowMount } from '@vue/test-utils';
+import HelloWorld from '@/components/HelloWorld.vue';
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+Vue.use(Vuetify);
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
+    const msg = 'Welcome to Vuetify';
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+    });
+    expect(wrapper.text()).toMatch(msg);
+  });
+});
