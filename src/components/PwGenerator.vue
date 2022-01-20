acdc8988fbc673e31b890ab59b127929133e92d0
kieperb <>
2022-01-20 17:19:54 +0100

    initial commit

---
+++ src/components/PwGenerator.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-container>
+    <v-form>
+      <v-row>
+        <v-col
+          cols="12"
+          sm="6"
+        >
+          <v-text-field
+            value="!§$%/()=*-_.:,;<>"
+            label="Sonderzeichen"
+            clearable
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col>
+           <v-slider
+
+        :tick-labels="ticksLabels"
+        :max="20"
+        :min="8"
+        step="1"
+        ticks="always"
+        tick-size="4"
+      ></v-slider>
+
+        </v-col>
+
+        <v-col>
+
+        </v-col>
+
+
+      </v-row>
+
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+name: 'PwGenerator',
+data: () => (
+       {
+        ticksLabels: [
+          8,
+          9,
+          10,
+          11,
+          12,
+13, 14,15,16,17,18,19,20
+        ],
+      }
+    )
+}
+</script>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,5 +1,8 @@
 import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
+import PwGenerator from '@/components/PwGenerator.vue';
+import App from '@/App.vue';
+
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 Vue.use(Vuetify);
@@ -11,5 +14,26 @@ describe('HelloWorld.vue', () => {
       propsData: { msg }
     });
     expect(wrapper.text()).toMatch(msg);
+  });
+});
+
+describe('App.vue', () => {
+  it('checks if pwgenerator is present in App', () => {
+    const wrapper = shallowMount(App);
+    expect(wrapper.findComponent({name: "PwGenerator"}).exists()).toBe(true);
+  });
+});
+
+describe('PwGenerator.vue', () => {
+  it('checks if special characters filed is present in PwGenerator', () => {
+    const wrapper = shallowMount(PwGenerator);
+    expect(wrapper.get("[label=Sonderzeichen]").exists()).toBe(true);
+  });
+});
+
+describe('PwGenerator.vue', () => {
+  it('checks if slider is present in PwGenerator', () => {
+    const wrapper = shallowMount(PwGenerator);
+    expect(wrapper.get("[label=Passwortlänge]").exists()).toBe(true);
   });
 });
