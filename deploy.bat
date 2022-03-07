83c373008864f0e27c4edfb26af1f4393ffbba01
kieperb <>
2022-03-07 23:43:29 +0100

    deploy automation

--- deploy.bat
+++ deploy.bat
@@ -4,5 +4,7 @@ call npm run build
 cd dist
 
 git init
-git add -addgit commit -m 'New Deplyment'
-git push -f git@github.com:kieperb/pw-generator-demo.git main:gh-pages
+git add -A
+git commit -m "New Deployment"
+git push -f git@github.com:kieperb/pw-generator-demo.git master:main
+cd ..
